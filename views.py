1e2c390282db40416a5286f5784ab7e16878d5e4
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-31 18:07:48 +0300

    added init.py file

--- views.py
+++ views.py
@@ -1,35 +1,37 @@
+
 from flask import Flask, render_template
-from newsapi import NewsApiclient
+from newsapi import NewsApiClient
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    newapi = NewsApiclient(api_key="6c9aad01acf44f5593688f40d255c0ca")
-    topheadlines = newapi.get_top_headlines(sources="al-jazeera-english")
+    newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
+    topheadlines = newapi.get_top_headlines(sources="ABC News")
 
     articles = topheadlines['articles']
     desc = []
     news = []
     img = []
     time = []
-    contet = []
+    content = []
     link = []
 
 
     for i in range(len(articles)):
-        myarticles - articles[i]
+        myarticles = articles[i]
         news.append(myarticles['title'])
         desc.append(myarticles['description'])
-        img.append(myarticles[''])
-        time.append(myarticles[''])
-        contet.append(myarticles[''])
-        link.append(myarticles[''])
+        img.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+        link.append(myarticles['url'])
 
 
-        my_list = zip(desc,contet,img)
+        my_list = zip(desc,content,img,news,link,time)
         return render_template('index.html',context=my_list)
 
-        if __name__   == "__main__":
-            app.run(debug=True)
+        if __name__ == "__main__":
+            app.run(debug = True)
