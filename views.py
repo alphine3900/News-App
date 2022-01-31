49d5c28b43a70bc7152ef82b3d2fed05699732d2
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-01-31 22:53:31 +0300

    modified home.py

--- views.py
+++ views.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
-    topheadlines = newapi.get_top_headlines(sources="al-jazeera-english,bbc-news")
+    topheadlines = newapi.get_top_headlines(sources="al-jazeera-english")
 
     articles = topheadlines['articles']
     description = []
@@ -33,5 +33,36 @@ def index():
         my_list = zip(description,content,img,news)
         return render_template('index.html', context=my_list)
 
+
+@app.route('/home')
+def home():
+    newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
+    topheadlines = newapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+    description = []
+    news = []
+    img = []
+    # time = []
+    content = []
+    # link = []
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        img.append(myarticles['urlToImage'])
+        # time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+        # link.append(myarticles['url'])
+
+
+        my_list = zip(description,content,img,news)
+        return render_template('home.html', context=my_list)
+
+# @app.route('/home')
+# def home():
+#     return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug = True)   
