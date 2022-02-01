ea812638f91feb1fd85d24b9eb889fc5c512b1ab
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-01 11:06:15 +0300

    modified health page

--- views.py
+++ views.py
@@ -105,5 +105,34 @@ def entertainment():
     my_list = zip( news,description,link,img,time,content)
     return render_template('entertainment.html', context=my_list)
 
+
+@app.route('/health')
+def health():
+    newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
+    topheadlines = newapi.get_top_headlines(sources="News24")
+
+    articles = topheadlines['articles']
+    news = []
+    description = []
+    link = []
+    img = []
+    time = []
+    content = []
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+       
+
+
+    my_list = zip( news,description,link,img,time,content)
+    return render_template('health.html', context=my_list)
+
 if __name__ == '__main__':
     app.run(debug = True)   
