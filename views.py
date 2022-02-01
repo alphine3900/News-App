f9d28b087f5b6aa877393f38447f0f279302211d
alphine3900 <alphine.kirimi@student.moringaschool.com>
2022-02-01 10:37:43 +0300

    modified sports page

--- views.py
+++ views.py
@@ -37,10 +37,48 @@ def index():
         return render_template('index.html', context=my_list)
 
 
+
 @app.route('/home')
 def home():
     newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
     topheadlines = newapi.get_top_headlines(sources="bbc-news")
+
+    articles = topheadlines['articles']
+
+    news = []
+    description = []
+    link = []
+    img = []
+    time = []
+    content = []
+   
+
+
+    for i in range(len(articles)):
+        myarticles = articles[i]
+        news.append(myarticles['title'])
+        description.append(myarticles['description'])
+        link.append(myarticles['url'])
+        img.append(myarticles['urlToImage'])
+        time.append(myarticles['publishedAt'])
+        content.append(myarticles['content'])
+       
+
+
+        
+    my_list = zip( news,description,link,img,time,content)
+    return render_template('home.html', context=my_list)
+
+
+
+
+
+
+
+@app.route('/sports')
+def sports():
+    newapi = NewsApiClient(api_key="6c9aad01acf44f5593688f40d255c0ca")
+    topheadlines = newapi.get_top_headlines(sources="ESPN")
 
     articles = topheadlines['articles']
     news = []
@@ -63,10 +101,7 @@ def home():
 
 
     my_list = zip( news,description,link,img,time,content)
-    return render_template('home.html', context=my_list)
+    return render_template('sports.html', context=my_list)
 
-# @app.route('/home')
-# def home():
-#     return render_template('home.html')
 if __name__ == '__main__':
     app.run(debug = True)   
